6c594a1b8db9a200d4ffbd5dc78c190c8883d57e
onis2 <nskrvyt@gmail.com>
2026-02-28 01:11:51 +0700

    feat: successfully integrate starlight and openapi for ai docs hub

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
 import mdx from '@astrojs/mdx';
+import starlight from '@astrojs/starlight';
+import starlightOpenAPI, { openAPISidebarGroups } from 'starlight-openapi';
 
 export default defineConfig({
   vite: {
@@ -15,5 +17,29 @@ export default defineConfig({
     rehypePlugins: [rehypeKatex],
   },
 
-  integrations: [mdx()]
+  integrations: [
+    starlight({
+      title: 'API Hub',
+      // By default, Starlight will style its own pages and won't be affected by your global.css
+      // because Starlight pages don't import your Layout.astro.
+      /*
+      sidebar: [
+        {
+          label: 'API Reference',
+          items: openAPISidebarGroups,
+        }
+      ],
+      plugins: [
+        starlightOpenAPI([
+          {
+            base: 'docs/api',
+            label: 'API',
+            schema: './src/content/docs/api/schema.yaml',
+          },
+        ]),
+      ],
+      */
+    }),
+    mdx()
+  ]
 });
